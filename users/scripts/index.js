0b602c3b36cab71834a4d6e66364e03c34732185
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-05-31 15:28:58 +0300

    Changing the buttons in the main page if the user is logged in

--- users/scripts/index.js
+++ users/scripts/index.js
@@ -1,3 +1,23 @@
+window.onload=checkIfLoggedIn;
+let login=document.getElementById("login");
+
+let signup=document.getElementById("signup");
+
+let edit=document.getElementById("edit");
+
+function checkIfLoggedIn() {
+    
+    if(getCookie("user_id")!== null){
+
+        signup.style.visibility="hidden";
+
+        login.innerText="SignOut";
+    }}
+
+
+
+
+
 let view_restaurants=document.getElementById('view_restaurants');
 
 view_restaurants.addEventListener("click", function(event){
@@ -5,3 +25,16 @@ view_restaurants.addEventListener("click", function(event){
     event.preventDefault();
 
     window.location.href = "http://localhost/myphpprojects/FrontEnd-Brisk/users/pages/view-restaurants.html";});
+
+
+
+    function getCookie(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0;i < ca.length;i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1,c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+        }
+        return null;
+      }
---
+++ users/styles/view-restaurants.js
@@ -0,0 +1,63 @@
+////
+window.onload=viewRestaurants;
+////
+
+let flex_item=document.getElementById("flex-item");
+
+function viewRestaurants() {
+    
+    if(getCookie("user_id")==null){
+
+        window.location.href = "http://localhost/myphpprojects/FrontEnd-Brisk/users/pages/login.html";
+
+    }
+   
+    var url = 'http://localhost/myphpprojects/BackEnd-Brisk/apis/get_restaurants.php';
+
+    axios({
+   
+        method: 'GET',
+   
+        url: url,
+   
+   
+    })
+
+    .then(function (response) {
+   
+        let result = response.data;
+
+        console.log(result);
+        
+        var string="";
+
+        result.forEach(element=>{
+                string += '<ul>'+ '<li>' + element.trade_name + '</li>' + '<li>' +
+                    element.working_hours + '</li>' +'<li>' + element.dining_room_capacity + '</li>'+'<li>' + element.phone + '</li>' +'<li>' + element.drivethrough_availabilty
+                    + '</li>' +'<li>' + element.delivery_availabilty + '</li>' +
+                    '<li>' + element.reservation_availabilty + '</li>' +'<li>' + element.address + '</li>' +'<li>' + element.support_all_diets+ '</li>'+'</ul>'
+    });
+
+flex_item.innerHTML=string; 
+
+
+});
+
+            
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+  }
+
+      
+
+
+
